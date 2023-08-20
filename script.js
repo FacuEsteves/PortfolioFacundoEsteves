309722f9fb06008b2ec2616f8d5b5ae38b7f0c26
Facundo Esteves <97993706+FacuEsteves@users.noreply.github.com>
2023-08-20 20:58:40 -0300

    Se agrego nueva informacion

--- script.js
+++ script.js
@@ -1,53 +1,81 @@
 let menuVisible = false;
 
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
-        menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
-        menuVisible = true;
-    }
-}
-
-function seleccionar(){
-
+function mostrarOcultarMenu() {
+  if (menuVisible) {
     document.getElementById("nav").classList = "";
     menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
 }
 
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("genexus");
-        habilidades[3].classList.add("csharp");
-        habilidades[4].classList.add("mssql");
-        habilidades[5].classList.add("ws");
-        habilidades[6].classList.add("comunicacion");
-        habilidades[7].classList.add("trabajo");
-        habilidades[8].classList.add("dedicacion");
-    }
+function seleccionar() {
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
 }
 
+function efectoHabilidades() {
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progreso");
+    habilidades[0].classList.add("javascript");
+    habilidades[1].classList.add("htmlcss");
+    habilidades[2].classList.add("genexus");
+    habilidades[3].classList.add("csharp");
+    habilidades[4].classList.add("mssql");
+    habilidades[5].classList.add("ws");
+    habilidades[6].classList.add("comunicacion");
+    habilidades[7].classList.add("trabajo");
+    habilidades[8].classList.add("dedicacion");
+  }
+}
 
-window.onscroll = function(){
-    efectoHabilidades();
-} 
+window.onscroll = function () {
+  efectoHabilidades();
+};
 
-document.getElementById("downloadButton").addEventListener("click", function() {
-
-    var fileUrl = "file/CV Facundo Esteves.pdf";
+document
+  .getElementById("downloadButton1")
+  .addEventListener("click", function () {
+    var fileUrl = "file/CV Facundo Esteves esp.pdf";
 
     var fileName = "CV_Facundo_Esteves.pdf";
 
     var link = document.createElement("a");
     link.href = fileUrl;
-    link.download = fileName;  
-
+    link.download = fileName;
 
     link.click();
-});
+  });
+
+document
+  .getElementById("downloadButton2")
+  .addEventListener("click", function () {
+    var fileUrl = "file/CV Facundo Esteves Ing.pdf";
+
+    var fileName = "CV_Facundo_Esteves.pdf";
+
+    var link = document.createElement("a");
+    link.href = fileUrl;
+    link.download = fileName;
+
+    link.click();
+  });
+
+const repoOwner = "FacuEsteves";
+const repoName = "PortfolioFacundoEsteves";
+const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}`;
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    const lastModified = new Date(data.updated_at);
+    const lastModifiedText = document.getElementById("lastModifiedText");
+    lastModifiedText.textContent += lastModified.toLocaleString();
+  })
+  .catch((error) => {
+    console.error("Error al obtener la información del repositorio:", error);
+  });
